9433154797752fb153b4c91533ec4ffa773930cb
Brayams Gómez <brayams.gomez@guate5.com>
2024-01-15 23:39:18 -0600

    V1. Job, Queue y Migration para verificacion de correo electronico

---
+++ app/Mail/VerificationCode.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationCode extends Mailable
+{
+    use Queueable, SerializesModels;
+    public string $code;
+    public string $name;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(string $code, string $name)
+    {
+        $this->code = $code;
+        $this->name = $name;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return Envelope
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Código de verificación',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return Content
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mails.verification-code',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
